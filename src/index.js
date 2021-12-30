c72d3492090642d2baf34ebfba8810e4b9169a36
nerderlyne <shivanshityagi@icloud.com>
2021-12-30 20:18:23 +0530

    rm unused package

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import { ColorModeScript, theme } from '@chakra-ui/react';
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
   <>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </>,
   document.getElementById('root')
 );
