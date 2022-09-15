768bd4cc81debc096d684db1e403f0fcfbfe73c3
shiv <shivanshityagi@icloud.com>
2022-09-15 23:39:45 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <Layout heading="Home" content="Homepage of the legal engineering guild.">
       <div className="flex flex-col items-center justify-center h-screen">
         <div className="flex flex-col space-y-5">
-          <h1 className="text-6xl md:text-9xl font-extrabold">LexDAO</h1>
+          <h1 className="text-6xl md:text-9xl font-extrabold">Lex<a href="https://snapshot.org/#/lexdao.eth" target="_blank" rel="noreferrer" className="text-brand-900 hover:text-brand-800">DAO</a></h1>
           <p className="text-xl md:text-5xl font-bold">upgrading the legal tradition.</p>
           <p className="text-xl md:text-5xl font-bold">together.</p>
         </div>
--- pages/join.tsx
+++ pages/join.tsx
@@ -6,10 +6,10 @@ const Join: NextPage = () => {
   return (
     <Layout heading="Join" content="Homepage of the legal engineering guild.">
       <div className="w-[100vw] flex flex-row items-center justify-between h-screen">
-        <div className="w-1/2 p-20 flex flex-col align-center justify-center">
+        <div className="w-1/2 p-20 flex flex-col align-center justify-center space-y-3">
           <h1 className="text-6xl md:text-9xl font-extrabold">LexDAO</h1>
           <p className="text-xl md:text-5xl font-bold">upgrading the legal tradition.</p>
-          <p className="text-xl md:text-5xl font-bold">together.</p>
+          <p className="text-xl md:text-5xl font-bold text-brand-100">together.</p>
         </div>
         <JoinForm />
       </div>
