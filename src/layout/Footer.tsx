38596d67188eea26a2539aa1eb523d1fa13b6238
shiv <shivanshityagi@icloud.com>
2022-09-15 20:32:03 +0000

    update footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,17 +1,17 @@
 import Link from 'next/link'
 
 export default function Footer() {
-    return (
-        <footer className="w-max h-5 absolute bottom-0 left-1/2 right-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2">
-            We are not a law firm, or a substitute for an attorney or a law firm. Use of our products are governed by our{' '}
-            <Link href="/terms">
-                <a className="hover:text-sky-700 hover:underline">Terms of Use</a>
-            </Link>{' '}
-            and{' '}
-            <Link href="/privacy">
-                <a className="hover:text-sky-700 hover:underline">Privacy</a>
-            </Link>
-            .
-        </footer>
-    )
+  return (
+    <footer className="w-max h-5 absolute bottom-0 left-1/2 right-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2">
+      We are not a law firm, or a substitute for an attorney or a law firm. Use of our products are governed by our{' '}
+      <Link href="/terms">
+        <a className="hover:text-sky-700 hover:underline">Terms of Use</a>
+      </Link>{' '}
+      and{' '}
+      <Link href="/privacy">
+        <a className="hover:text-sky-700 hover:underline">Privacy</a>
+      </Link>
+      .
+    </footer>
+  )
 }
--- src/layout/Nav.tsx
+++ src/layout/Nav.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link'
 import Item from './Item'
-import { HiInformationCircle } from 'react-icons/hi'
 import { BsCalendar } from 'react-icons/bs'
 import { BiHome } from 'react-icons/bi'
 import { TbBooks } from 'react-icons/tb'
@@ -8,9 +6,9 @@ import { TbBooks } from 'react-icons/tb'
 export default function Nav() {
   return (
     <nav className="w-max fixed bottom-10 left-1/2 right-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2 rounded-xl flex flex-row justify-center align-center space-x-10 border-2 border-black">
-      <Item link="/" icon={<BiHome size={36} />} />
-      <Item link="/events" icon={<BsCalendar size={36} />} />
-      <Item link="/library" icon={<TbBooks size={36} />} />
+      <Item link="/" icon={<BiHome size={36} className="hover:animate-bounce" />} />
+      <Item link="/events" icon={<BsCalendar size={36} className="hover:animate-bounce" />} />
+      <Item link="http://lib.lexdao.coop/" icon={<TbBooks size={36} className="hover:animate-bounce" />} />
     </nav>
   )
 }
