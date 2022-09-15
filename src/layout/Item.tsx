33341225ee7b2174c342fb4eac05913d2be4c784
shiv <shivanshityagi@icloud.com>
2022-09-15 20:40:24 +0000

    update

--- src/layout/Item.tsx
+++ src/layout/Item.tsx
@@ -4,7 +4,9 @@ import Lottie from 'react-lottie'
 type Props = {
   link: string
   icon: React.ReactNode
+  isExternal?: Boolean
 }
-export default function Home({ link, icon }: Props) {
+export default function Home({ link, icon, isExternal }: Props) {
+  if (isExternal === true) return <a href={link} target="_blank">{icon}</a>
   return <Link href={link}>{icon}</Link>
 }
