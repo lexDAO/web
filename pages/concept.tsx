d80c1d453dd9fd89ed1f423e2760a90dc77a6664
Shiv <shivanshityagi@icloud.com>
2022-10-04 21:07:28 +0900

    update formatting

--- pages/concept.tsx
+++ pages/concept.tsx
@@ -1,9 +1,9 @@
-import Spline from '@splinetool/react-spline';
+import Spline from '@splinetool/react-spline'
 
 export default function Concept() {
   return (
     <div>
-    <Spline scene="https://prod.spline.design/WwnlXg2MM6Wez68m/scene.splinecode" className="min-h-screen" />
+      <Spline scene="https://prod.spline.design/WwnlXg2MM6Wez68m/scene.splinecode" className="min-h-screen" />
     </div>
-  );
+  )
 }
