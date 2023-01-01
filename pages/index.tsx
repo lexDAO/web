ba4ac3e0bea0fc5bd2826df8d875bb1766968d22
Shiv <shivanshityagi@icloud.com>
2023-01-01 20:09:42 +0530

    update subdaos

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,14 @@ import Image from 'next/image'
 import { SplashText } from '~/lander'
 import Layout from '~/layout'
 import {
-  ArrowUturnLeftIcon,
+  BoltIcon,
   BookOpenIcon,
-  ChatBubbleLeftEllipsisIcon,
-  DocumentChartBarIcon,
-  HeartIcon,
-  InboxIcon,
+  BriefcaseIcon,
+  BuildingOffice2Icon,
+  Cog8ToothIcon,
+  GlobeAltIcon,
+  LightBulbIcon,
   PencilIcon,
-  PencilSquareIcon,
-  SparklesIcon,
-  TrashIcon,
-  UsersIcon,
   WrenchScrewdriverIcon,
 } from '@heroicons/react/20/solid'
 
@@ -23,18 +20,42 @@ const Home: NextPage = () => {
     {
       name: 'Network',
       description: 'Join LexDAO and get access to our network of people and resources.',
-      icon: InboxIcon,
+      icon: GlobeAltIcon,
     },
     {
       name: 'Innovate',
       description:
         'Impact legal processes and services by bringing traditional legal settlements on to the blockchain.',
-      icon: UsersIcon,
+      icon: BoltIcon,
     },
     {
       name: 'Grow',
       description: 'Grow your knowledge base in a radically new and emerging space',
-      icon: TrashIcon,
+      icon: LightBulbIcon,
+    },
+  ]
+
+  const projects = [
+    {
+      name: 'Kali',
+      description: 'The product suite for building DAOs and incorporation.',
+      href: 'https://kali.gg/',
+      cta: 'Learn more',
+      icon: Cog8ToothIcon,
+    },
+    {
+      name: 'Clinic',
+      description: 'A legal support network for DAOs. Apprentices welcome!',
+      href: 'https://discord.gg/nZVnxnfw54',
+      cta: 'Ask the Clinic',
+      icon: BriefcaseIcon,
+    },
+    {
+      name: 'RWA',
+      description: 'Building standards and community around digitizing assets.',
+      href: 'https://rwaconsortium.com/',
+      cta: 'Explore',
+      icon: BuildingOffice2Icon,
     },
   ]
 
@@ -194,7 +215,6 @@ const Home: NextPage = () => {
             Join us in securing rules and promises with code.
           </h2>
           <p className="mt-4 max-w-3xl text-lg text-brand-50">
-            {/* subtitle copy for 'Securing rules and promises with code.'   */}
             LexDAO is a community of legal engineers, lawyers, and technologists who are building the future of law.
           </p>
           <div className="mt-12 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-16">
@@ -211,6 +231,36 @@ const Home: NextPage = () => {
                 </div>
               </div>
             ))}
+          </div>
+        </div>
+        {/* add section to highlight projects */}
+        <div className="bg-gradient-to-r from-brand-600 to-brand-900">
+          <div className="mx-auto max-w-4xl px-4 py-16 sm:px-6 sm:pt-20 sm:pb-24 lg:max-w-7xl lg:px-8 lg:pt-24">
+            <h2 className="text-3xl font-bold tracking-tight text-white">SubDAOs</h2>
+            <p className="mt-4 max-w-3xl text-lg text-brand-50">
+              LexDAO spawns SubDAOs to build, deploy and maintain legal technology.
+            </p>
+            <div className="mt-12 grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-4 lg:gap-x-8 lg:gap-y-16">
+              {projects.map((project) => (
+                <div key={project.name}>
+                  <div>
+                    <span className="flex h-12 w-12 items-center justify-center rounded-md bg-white bg-opacity-10">
+                      <project.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                    </span>
+                  </div>
+                  <div className="mt-6">
+                    <h3 className="text-lg font-medium text-white">{project.name}</h3>
+                    <p className="mt-2 text-base text-brand-50">{project.description}</p>
+                    <a
+                      href={project.href}
+                      className="mt-2 inline-flex rounded-md border border-transparent bg-gradient-to-r from-zinc-50 to-zinc-200 bg-origin-border px-4 py-2 text-base font-medium text-brand-900 shadow-sm hover:from-zinc-300 hover:to-brand-50"
+                    >
+                      {project.cta}
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
